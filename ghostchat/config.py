d1718146297997afb31771f89483cd6628e0a46f
Henrique Brandão <brandao.t.henrique@gmail.com>
2025-09-08 21:22:09 -0300

    keep debugging

---
+++ ghostchat/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+class Config:
+    """
+    Class to include variables related to the application.
+    """
+    pass
+
+config = Config()
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gunicorn -w 3 -b :8000 -k uvicorn.workers.UvicornWorker -t 90 --preload --max-requests=500 ghostchat.app:app
+
+# gunicorn -w 3 -b :8000 -k uvicorn.workers.UvicornWorker -t 90 --preload --max-requests=500 ghostchat.app.main:app
+uvicorn ghostchat.app.main:app --reload --host 0.0.0.0 --port 8000
